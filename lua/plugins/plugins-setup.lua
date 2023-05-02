05d009e73cea38d88eaa1b9569064b76c955eb59
toast <2878428463@qq.com>
2023-05-02 11:13:34 +0800

    remove vimspector plugs

--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -73,7 +73,6 @@ return require('packer').startup(function(use)
         "iamcco/markdown-preview.nvim",
         run = function() vim.fn["mkdp#util#install"]() end,
   })
-  use "puremourning/vimspector" -- 断点调试
   if packer_bootstrap then
     require('packer').sync()
   end
